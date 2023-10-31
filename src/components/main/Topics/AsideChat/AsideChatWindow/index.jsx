67575b328e3cf3b5c0743781121e3a15cd629f1b
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2023-10-31 10:06:08 +0200

    added aside chat in topics

--- src/components/main/Topics/AsideChat/AsideChatWindow/index.jsx
+++ src/components/main/Topics/AsideChat/AsideChatWindow/index.jsx
@@ -5,7 +5,7 @@ export default function AsideChatWindow() {
     return (
         <div className={styles.asideChatWindow}>
             <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__personMessage}`}>Generate for me a coding course, beginner level, for 4 weeks </p>
-            <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__botMessage}`}>What you know</p>
+            <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__botMessage}`}>What you knowWhat you knowWhat you know</p>
             <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__personMessage}`}>Generate for me a coding course, beginner level, for 4 weeks </p>
             <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__botMessage}`}>What you know</p>
             <p className={`${styles.asideChatWindow__message} ${styles.asideChatWindow__personMessage}`}>Generate for me a coding course, beginner level, for 4 weeks </p>
--- src/components/main/Topics/AsideChat/AsideMessageInput/AsideInput/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './asideInput.module.scss'
-
-export default function AsideInput() {
-    return (
-        <input className={styles.asideMessageInput__input} type="text" placeholder='Type message' />
-    )
-}
