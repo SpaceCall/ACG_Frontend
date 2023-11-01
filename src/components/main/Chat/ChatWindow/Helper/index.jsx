d42ed5b32770c71ece6dc07d504e8373f730cf4e
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2023-11-01 21:01:05 +0200

    dddd

--- src/components/main/Chat/ChatWindow/Helper/index.jsx
+++ src/components/main/Chat/ChatWindow/Helper/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Loader from '../Loader'
-import styles from './helper.module.scss'
+
 export default function Helper({ text }) {
   const [displayText, setDisplayText] = useState('');
     const loader = <Loader />
--- src/components/main/Topics/index.jsx
+++ src/components/main/Topics/index.jsx
@@ -3,11 +3,12 @@ import styles from './topics.module.scss'
 import AsideChat from './AsideChat'
 import Content from './Content'
 import CoursePath from './CoursePath'
+import CourseBar from './CourseBar'
 
 export default function Topics() {
     return (
         <div className={styles.topics}>
-            <CoursePath />
+            <CourseBar />
             <Content />
             <AsideChat />
         </div>
