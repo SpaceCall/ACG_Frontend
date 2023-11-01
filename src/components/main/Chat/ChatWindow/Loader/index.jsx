ba729dcd982a6cbc3acbfc682fc275accb6b2e5b
Morzh7771 <imhoserge@gmail.com>
2023-11-01 20:14:25 +0200

    buba

---
+++ src/components/main/Chat/ChatWindow/Loader/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './loader.css'
+export default function Loader() {
+  return (
+    <span>
+        <div className='loadingio_spinner_rolling_9ba6accsutn'>
+            <div className='ldio_z8hlzitlxn'>
+                <div></div>
+    </div></div>
+    </span>
+  )
+}
--- src/components/main/Chat/ChatWindow/index.jsx
+++ src/components/main/Chat/ChatWindow/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import styles from './chatWindow.module.scss'
-
+import Helper from './Helper'
 export default function ChatWindow({ messages }) {
   const chatRef = useRef(null)
 
@@ -11,22 +11,20 @@ export default function ChatWindow({ messages }) {
       lastMessage.scrollIntoView({ behavior: 'smooth' })
     }
   }, [messages])
-  const elements = messages.map((item,id) => {
-    console.log(item.label)
-    if(item.isUserSend === true){
-        return (
-        <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__personMessage}`}>{item.label}</p>
-        );
-    }else{
-        return (
-            <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__botMessage}`}>{
-                setTimeout(()=>{
-                return item.label
-                },2)}
-            </p>
-        );
+
+
+  useEffect(() => {
+    const displayMessagesWithDelay = async () => {
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
     }
-  });
+    displayMessagesWithDelay()
+  }, [messages])
+function biba(){
+    setTimeout(()=>{
+        return 'negr'
+    },1000)
+}
   return (
     <div className={styles.chatWindow} ref={chatRef}>
       {messages.map((message, id) => {
@@ -36,14 +34,14 @@ export default function ChatWindow({ messages }) {
             );
         }else{
             return (
-                <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__botMessage}`}>{
-                    setTimeout(()=>{
-                    return message.label
-                    },2)}
-                </p>
+                <div key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__botMessage}`}><Helper text={message.label}/></div>
             );
         }
       })}
     </div>
   )
 }
+
+
+
+
