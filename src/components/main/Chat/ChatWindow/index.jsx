da140b73c69cae7b7a3f6430dbcc594124b027d6
Morzh7771 <imhoserge@gmail.com>
2023-11-01 18:29:23 +0200

    biba

--- src/components/main/Chat/ChatWindow/index.jsx
+++ src/components/main/Chat/ChatWindow/index.jsx
@@ -3,13 +3,22 @@ import styles from './chatWindow.module.scss'
 
 export default function ChatWindow({messages}) {
     const elements = messages.map((item,id) => {
-        return (
-          <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__personMessage}`}>{item.label}</p>
-        );
+        console.log(item.label)
+        if(item.isUserSend === true){
+            return (
+            <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__personMessage}`}>{item.label}</p>
+            );
+        }else{
+            return (
+                <p key={id} className={`${styles.chatWindow__message} ${styles.chatWindow__botMessage}`}>{
+                    setTimeout(()=>{
+                    return item.label
+                    },2)}
+                </p>
+            );
+        }
       });
-      useEffect (() =>{
-        window.scroll(0,9999999)
-      },[messages])
+
     return (
         <div className={styles.chatWindow}>
            {elements}
--- src/components/main/Chat/index.jsx
+++ src/components/main/Chat/index.jsx
@@ -5,13 +5,21 @@ import ChatWindow from './ChatWindow'
 
 export default function Chat() {
     const [messages,setMessages] = useState([])
-    function createTodoItem(label) {
+    const [index,setIndex] = useState(0);
+    const botMessages = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17']
+    function createUserMessages(label) {
         return {
             label,
             isUserSend:true,
         }
     }
-    const sendMassage = (text) =>{ if(text.length >= 1) setMessages([...messages,createTodoItem(text)]) }
+    function createBotMessages() {
+        return {
+            label:botMessages[index],
+            isUserSend:false,
+        }
+    }
+    const sendMassage = (text) =>{ if(text.length >= 1) setMessages([...messages,createUserMessages(text),createBotMessages()]);setIndex(index + 1) }
     return (
         <div className={styles.chat}>
             <ChatWindow messages={messages}/>
