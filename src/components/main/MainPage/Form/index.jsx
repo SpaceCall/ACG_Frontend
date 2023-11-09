83c6237fd44b5dfd55f6e096f08c80dac659c1ba
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2023-11-09 19:13:16 +0200

    form block

--- src/components/main/MainPage/Form/index.jsx
+++ src/components/main/MainPage/Form/index.jsx
@@ -3,6 +3,13 @@ import styles from './form.module.scss'
 
 export default function Form() {
     return (
-        <div id='form' className={styles.form}>Form</div>
+        <div id='form' className={styles.form}>
+            <h2>Join the ACG Community</h2>
+            <p>Become a part of the <span>ACG</span> community and gain access to personalized learning like never before. Sign up now to start your journey toward knowledge, growth, and success.</p>
+            <form>
+                <input type="text" placeholder='Enter your email' />
+                <a href='#'>Send</a>
+            </form>
+        </div>
     )
 }
