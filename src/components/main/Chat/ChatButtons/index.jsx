18d2ce93d40fafbb0de231ac78cd31e9eff58ba7
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2023-11-09 18:24:20 +0200

    fixed bugs

--- src/components/main/Chat/ChatButtons/index.jsx
+++ src/components/main/Chat/ChatButtons/index.jsx
@@ -2,27 +2,23 @@ import React, { useState, useEffect } from 'react'
 import styles from './chatButtons.module.scss'
 import { NavLink } from 'react-router-dom'
 
-export default function ChatButtons({time}) {
+export default function ChatButtons({ time }) {
 
-    const [displayContent, setDisplayContent] = useState('')
-    const button = <div className={styles.chatButtons}>
-    <button className={styles.chatButtons__cancel}>
-        <span>No, I want to change something</span>
-    </button>
-    <button className={styles.chatButtons__confirm}>
-        <NavLink to={"/topics"}>Yes, generate course</NavLink>
-    </button>
-</div>
-    useEffect(() => {
-        setDisplayContent('')
-      const timer = setTimeout(() => {
-        setDisplayContent(button)
-      },time)
-      return () => {
-        clearTimeout(timer)
-      }
-    },[])
-    return (
-        <div>{displayContent}</div>
-    )
+  const [displayContent, setDisplayContent] = useState('')
+  const button = <div className={styles.chatButtons}>
+    <a className={styles.chatButtons__cancel}>No, I want to change something</a>
+    <NavLink to={"/topics"} className={styles.chatButtons__confirm}>Yes, generate course</NavLink>
+  </div>
+  useEffect(() => {
+    setDisplayContent('')
+    const timer = setTimeout(() => {
+      setDisplayContent(button)
+    }, time)
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [])
+  return (
+    <div>{displayContent}</div>
+  )
 }
