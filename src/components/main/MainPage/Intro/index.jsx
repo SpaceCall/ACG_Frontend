a9a8fcd70d8c953c89b507dfa0987d54359bd5b8
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2023-11-15 11:41:18 +0200

    dgrher

--- src/components/main/MainPage/Intro/index.jsx
+++ src/components/main/MainPage/Intro/index.jsx
@@ -5,11 +5,10 @@ export default function Intro() {
     return (
         <div id='intro' className={styles.intro}>
             <div className="container">
-                <div className={styles.intro__personalized}>
-                    <p>Personalized</p>
-                    <p>Courses Tailored Just for You</p>
-                </div>
                 <h1>Unlock Your Learning Potential with ACG</h1>
+                <div className={styles.intro__personalized}>
+                    <p>Personalized сourses Tailored Just for You</p>
+                </div>
                 <div className={styles.intro__buttons}>
                     <a href='#form' className={styles.intro__buttons__course}>Get a demo</a>
                     <a href='#howitworks' className={styles.intro__buttons__learn}>Learn more</a>
