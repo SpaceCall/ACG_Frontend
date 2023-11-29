54c8d0daa8e0e80b3237572b1bf303019018d9d2
SpaceCall <93478572+SpaceCall@users.noreply.github.com>
2023-11-29 13:10:39 +0200

    Update mainPage Visit

--- src/components/main/MainPage/index.jsx
+++ src/components/main/MainPage/index.jsx
@@ -8,8 +8,31 @@ import Form from './Form'
 import Contacts from './Contacts'
 
 export default function MainPage() {
+    let visit = async () => {
+        try {
+            const ipResponse = await axios.get(`https://api.ipify.org?format=json`);
+            const ip = ipResponse.data.ip;
+            const csrfResponse = await axios.get(`https://artcogen.com/csrf-token`, {
+                withCredentials: true,
+            });
+            const csrfToken = csrfResponse.data.csrfToken;
+            const response = await axios.post(
+                `https://artcogen.com/landing/visit`,
+                {},
+                {
+                    headers: {
+                        'X-CSRF-TOKEN': csrfToken,
+                        'ip': ip
+                    },
+                    withCredentials: true,
+                    }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
-        <div className={styles.mainPage}>
+        <div className={styles.mainPage} onLoad={visit}>
             <div className={styles.mainPage__wrapper}>
                 <Intro />
                 <Cases />
