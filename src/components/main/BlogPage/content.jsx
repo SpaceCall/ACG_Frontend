d3ed783f21dbacd9f24b39b5f47a31eae5a9b350
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-03-21 15:22:19 +0200

    added content without styles

--- src/components/main/BlogPage/content.jsx
+++ src/components/main/BlogPage/content.jsx
@@ -24,7 +24,23 @@ export default function Content({ styles }) {
                     ))}
                 </ul>
             </div>
-            <div className={styles.blog__content__text}></div>
+            <div className={styles.blog__content__text}>
+                {data.contentItems.map((textBlock) => (
+                    <div key={textBlock.id} className={styles.blog__content__text__block}>
+                        <h2 className={styles.blog__content__text__block__title}>{textBlock.title && textBlock.title}</h2>
+                        <h3 className={styles.blog__content__text__block__subtitle}>{textBlock.subtitleTop && textBlock.subtitleTop}</h3>
+                        <p className={styles.blog__content__text__block__paragraph}>{textBlock.description && textBlock.description}</p>
+                        <h3 className={styles.blog__content__text__block__subtitle}>{textBlock.subtitleBottom && textBlock.subtitleBottom}</h3>
+                        <div className={styles.blog__content__text__block__code}>
+                            <ul>
+                                {textBlock.code && textBlock.code.map((codeStr, index) => (
+                                    <li key={index}>{codeStr}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
