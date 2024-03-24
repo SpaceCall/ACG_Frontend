ebb16869cb85e746bea941ceee5c8108e7baf323
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-03-24 14:28:48 +0200

    changed cases and HIW

--- src/components/main/MainPage/Cases/index.jsx
+++ src/components/main/MainPage/Cases/index.jsx
@@ -1,160 +1,54 @@
-import React, { useRef, useEffect } from 'react'
-import Slider from 'react-slick'
+import React from 'react'
 import styles from './cases.module.scss'
-import 'slick-carousel/slick/slick-theme.scss'
-import 'slick-carousel/slick/slick.scss'
-import case_image from './../../../../assets/images/case-image.png'
 
 export default function Cases() {
-    const casesData = [
+    const list = [
         {
-            title: ['Tailoring', 'Education'],
-            list: [
-                'ACG will design a learning plan based on your needs and experience.',
-                'ACG will design a learning plan based on your needs and experience.',
-            ],
+            title: ["Tailoring", "Education"],
+            description: <>
+                Based on your experience, ACG will generate a course that will provide the necessary knowledge to achieve your goal
+            </>
         },
         {
-            title: ['Task-Oriented', 'Learning'],
-            list: [
-                'We will give detailed practical assignments, so that you do not just learn the theory, but immediately apply it on real tasks.',
-                'You can tell us what you are looking for from this course, what skills you are interested in, and we will provide material especially to prepare you for the assignment.',
-            ],
+            title: ["Real-projects", "practice"],
+            description: <>
+                Consolidate your knowledge by completing advanced practice exercises that will prepare you for real work tasks
+            </>
         },
         {
-            title: ['Individual', 'Approach'],
-            list: [
-                'The platform analyzes your practice results, as well as learning difficulties and questions you asked the bot throughout the course.',
-                'Based on this information, ACG updates your training plan to help you understand the material.',
-                'You will always have a bot mentor to explain everything you don\'t understand.',
-            ],
+            title: ["Individual", "support"],
+            description: <>
+                Receive support from a mentor-chatbot during the course and practical tasks. Based on your questions and results, ACG will adapt the next material.
+            </>
         },
     ]
 
-    const debounce = (func, wait, immediate) => {
-        let timeout
-        return function (...args) {
-            const context = this
-            const later = () => {
-                timeout = null
-                if (!immediate) func.apply(context, args)
-            }
-            const callNow = immediate && !timeout
-            clearTimeout(timeout)
-            timeout = setTimeout(later, wait)
-            if (callNow) func.apply(context, args)
-        }
-    }
-
-    const sliderSettings = {
-        vertical: true,
-        adaptiveHeight: false,
-        swipe: false,
-        infinite: false,
-        arrows: false
-    }
-
-    const slickRef = useRef(null)
-    const slickIsAnimating = useRef(false)
-
-    const handleWheelEvent = debounce((e) => {
-        if (!slickIsAnimating.current) {
-            e.preventDefault()
-            const direction = Math.abs(e.deltaX) > Math.abs(e.deltaY) ? e.deltaX : e.deltaY
-            direction > 0 ? slickRef.current.slickNext() : slickRef.current.slickPrev()
-        }
-    }, 100, true)
-
-    useEffect(() => {
-        slickRef.current.slickGoTo(0)
-
-        const listElement = slickRef.current.innerSlider.list
-        listElement.addEventListener('wheel', handleWheelEvent)
-
-        return () => {
-            listElement.removeEventListener('wheel', handleWheelEvent)
-        }
-    }, [handleWheelEvent]);
-
     return (
         <div id='cases' className={styles.cases}>
-            <div className='container'>
-                <h2 className={styles.cases__title}>Use Cases</h2>
-                <Slider className={styles.cases__wrapper} ref={slickRef} {...sliderSettings}>
-                    {casesData.map((caseItem, index) => (
-                        <div key={index}>
-                            <div
-                                key={index}
-                                className={`
-                                    ${styles.cases__case} 
-                                    ${index % 2 === 0 ? styles.cases__odd :
-                                                        styles.cases__even}
-                                `}
-                            >
-                                <div
-                                    className={`
-                                        ${styles.cases__case__image} 
-                                        ${index % 2 === 0 ?
-                                                        styles.cases__odd__image :
-                                                        styles.cases__even__image}
-                                    `}
-                                >
-                                    <img src={case_image} alt='Chat' />
-                                </div>
-                                <div
-                                    className={`
-                                        ${styles.cases__case__description} 
-                                        ${index % 2 === 0 ? styles.cases__odd__description :
-                                                            styles.cases__even__description}
-                                    `}
-                                >
-                                    <h3
-                                        className={`
-                                            ${styles.cases__case__description__title} 
-                                            ${index % 2 === 0 ? styles.cases__odd__description__title :
-                                                                styles.cases__even__description__title}
-                                        `}
-                                    >
-                                        <span
-                                            className={`
-                                                ${styles.cases__case__description__title__orange} 
-                                                ${index % 2 === 0 ? styles.cases__odd__description__title__orange :
-                                                                    styles.cases__even__description__title__orange}
-                                        `}
-                                        >
-                                            {caseItem.title[0]}
+            <div className="container">
+                <h2>Use  <span>cases</span></h2>
+                <div className={styles.cases__wrapper}>
+                    <ul className={styles.cases__list}>
+                        {list.map((listItem, index) => (
+                            <li key={index} className={styles.cases__list__item}>
+                                <div className={styles.cases__list__item__info}>
+                                    <h3>
+                                        <span>
+                                            {listItem.title[0]}
                                         </span>
-                                        <span
-                                            className={`
-                                                ${styles.cases__case__description__title__black} 
-                                                ${index % 2 === 0 ? styles.cases__odd__description__title__black :
-                                                                    styles.cases__even__description__title__black}
-                                            `}
-                                        >
-                                            {caseItem.title[1]}
+                                        <span>
+                                            {listItem.title[1]}
                                         </span>
                                     </h3>
-                                    <ul
-                                        className={`
-                                            ${styles.cases__case__description__list} 
-                                            ${index % 2 === 0 ? styles.cases__odd__description__list :
-                                                                styles.cases__even__description__list}`}
-                                    >
-                                        {caseItem.list.map((item, listItemIndex) => (
-                                            <li key={listItemIndex}>
-                                                <div className={styles.line}>
-                                                    <hr />
-                                                </div>
-                                                {item}
-                                            </li>
-                                        ))}
-                                    </ul>
+                                    <p>
+                                        {listItem.description}
+                                    </p>
                                 </div>
-                            </div>
-                        </div>
-                    ))}
-                </Slider>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
-    );
+    )
 }
--- src/components/main/MainPage/HowItWorks/index.jsx
+++ src/components/main/MainPage/HowItWorks/index.jsx
@@ -1,54 +1,160 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import Slider from 'react-slick'
 import styles from './howItWorks.module.scss'
+import 'slick-carousel/slick/slick-theme.scss'
+import 'slick-carousel/slick/slick.scss'
+import case_image from './../../../../assets/images/case-image.png'
 
 export default function HowItWorks() {
-    const list = [
+    const casesData = [
         {
-            title: ["Choose Your", "Learning Path"],
-            description: <>
-                Dive into the world of knowledge by selecting the topic that ignites your curiosity. With a vast array of subjects to explore, <span>ACG</span> ensures you have the freedom to learn what matters most to you. Say exactly what you expect from the course, your current level of knowledge, and the time you want to invest in it.
-            </>
+            title: ['Tailoring', 'Education'],
+            list: [
+                'ACG will design a learning plan based on your needs and experience.',
+                'ACG will design a learning plan based on your needs and experience.',
+            ],
         },
         {
-            title: ["Customize Your", "Learning Experience"],
-            description: <>
-                Dive into the world of knowledge by selecting the topic that ignites your curiosity. With a vast array of subjects to explore, <span>ACG</span> ensures you have the freedom to learn what matters most to you. Say exactly what you expect from the course, your current level of knowledge, and the time you want to invest in it.
-            </>
+            title: ['Task-Oriented', 'Learning'],
+            list: [
+                'We will give detailed practical assignments, so that you do not just learn the theory, but immediately apply it on real tasks.',
+                'You can tell us what you are looking for from this course, what skills you are interested in, and we will provide material especially to prepare you for the assignment.',
+            ],
         },
         {
-            title: ["Your Comprehensive", "Learning Journey"],
-            description: <>
-                Dive into the world of knowledge by selecting the topic that ignites your curiosity. With a vast array of subjects to explore, <span>ACG</span> ensures you have the freedom to learn what matters most to you. Say exactly what you expect from the course, your current level of knowledge, and the time you want to invest in it.
-            </>
+            title: ['Individual', 'Approach'],
+            list: [
+                'The platform analyzes your practice results, as well as learning difficulties and questions you asked the bot throughout the course.',
+                'Based on this information, ACG updates your training plan to help you understand the material.',
+                'You will always have a bot mentor to explain everything you don\'t understand.',
+            ],
         },
     ]
 
+    const debounce = (func, wait, immediate) => {
+        let timeout
+        return function (...args) {
+            const context = this
+            const later = () => {
+                timeout = null
+                if (!immediate) func.apply(context, args)
+            }
+            const callNow = immediate && !timeout
+            clearTimeout(timeout)
+            timeout = setTimeout(later, wait)
+            if (callNow) func.apply(context, args)
+        }
+    }
+
+    const sliderSettings = {
+        vertical: true,
+        adaptiveHeight: false,
+        swipe: false,
+        infinite: false,
+        arrows: false
+    }
+
+    const slickRef = useRef(null)
+    const slickIsAnimating = useRef(false)
+
+    const handleWheelEvent = debounce((e) => {
+        if (!slickIsAnimating.current) {
+            e.preventDefault()
+            const direction = Math.abs(e.deltaX) > Math.abs(e.deltaY) ? e.deltaX : e.deltaY
+            direction > 0 ? slickRef.current.slickNext() : slickRef.current.slickPrev()
+        }
+    }, 100, true)
+
+    useEffect(() => {
+        slickRef.current.slickGoTo(0)
+
+        const listElement = slickRef.current.innerSlider.list
+        listElement.addEventListener('wheel', handleWheelEvent)
+
+        return () => {
+            listElement.removeEventListener('wheel', handleWheelEvent)
+        }
+    }, [handleWheelEvent]);
+
     return (
-        <div id='howitworks' className={styles.howItWorks}>
-            <div className="container">
-                <h2>How it works <span>?</span></h2>
-                <div className={styles.howItWorks__wrapper}>
-                    <ul className={styles.howItWorks__list}>
-                        {list.map((listItem, index) => (
-                            <li key={index} className={styles.howItWorks__list__item}>
-                                <div className={styles.howItWorks__list__item__info}>
-                                    <h3>
-                                        <span>
-                                            {listItem.title[0]}
+        <div id='howItWorks' className={styles.cases}>
+            <div className='container'>
+                <h2 className={styles.cases__title}>Use Cases</h2>
+                <Slider className={styles.cases__wrapper} ref={slickRef} {...sliderSettings}>
+                    {casesData.map((caseItem, index) => (
+                        <div key={index}>
+                            <div
+                                key={index}
+                                className={`
+                                    ${styles.cases__case} 
+                                    ${index % 2 === 0 ? styles.cases__odd :
+                                                        styles.cases__even}
+                                `}
+                            >
+                                <div
+                                    className={`
+                                        ${styles.cases__case__image} 
+                                        ${index % 2 === 0 ?
+                                                        styles.cases__odd__image :
+                                                        styles.cases__even__image}
+                                    `}
+                                >
+                                    <img src={case_image} alt='Chat' />
+                                </div>
+                                <div
+                                    className={`
+                                        ${styles.cases__case__description} 
+                                        ${index % 2 === 0 ? styles.cases__odd__description :
+                                                            styles.cases__even__description}
+                                    `}
+                                >
+                                    <h3
+                                        className={`
+                                            ${styles.cases__case__description__title} 
+                                            ${index % 2 === 0 ? styles.cases__odd__description__title :
+                                                                styles.cases__even__description__title}
+                                        `}
+                                    >
+                                        <span
+                                            className={`
+                                                ${styles.cases__case__description__title__orange} 
+                                                ${index % 2 === 0 ? styles.cases__odd__description__title__orange :
+                                                                    styles.cases__even__description__title__orange}
+                                        `}
+                                        >
+                                            {caseItem.title[0]}
                                         </span>
-                                        <span>
-                                            {listItem.title[1]}
+                                        <span
+                                            className={`
+                                                ${styles.cases__case__description__title__black} 
+                                                ${index % 2 === 0 ? styles.cases__odd__description__title__black :
+                                                                    styles.cases__even__description__title__black}
+                                            `}
+                                        >
+                                            {caseItem.title[1]}
                                         </span>
                                     </h3>
-                                    <p>
-                                        {listItem.description}
-                                    </p>
+                                    <ul
+                                        className={`
+                                            ${styles.cases__case__description__list} 
+                                            ${index % 2 === 0 ? styles.cases__odd__description__list :
+                                                                styles.cases__even__description__list}`}
+                                    >
+                                        {caseItem.list.map((item, listItemIndex) => (
+                                            <li key={listItemIndex}>
+                                                <div className={styles.line}>
+                                                    <hr />
+                                                </div>
+                                                {item}
+                                            </li>
+                                        ))}
+                                    </ul>
                                 </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                            </div>
+                        </div>
+                    ))}
+                </Slider>
             </div>
         </div>
-    )
+    );
 }
