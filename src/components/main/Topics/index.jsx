cbf8d2f4298d5be2a8c40b425abbacdbc7124be4
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-07-31 16:54:36 +0300

    support window and beggining of adaptation

--- src/components/main/Topics/index.jsx
+++ src/components/main/Topics/index.jsx
@@ -5,10 +5,14 @@ import CoursePath from './CoursePath';
 import CourseBar from './CourseBar';
 import ActiveCourse from './Content/activeCourse';
 import data from './Content/data.json';
+import support_icon from '../../../assets/icons/support_icon.svg'
+import close_support_icon from '../../../assets/icons/close_support_icon.svg'
+import SupportWindow from './SupportWindow';
 
 export default function Topics() {
     const [isActive, setIsActive] = useState({});
     const [coursesData, setCoursesData] = useState([]);
+    const [isSupportOpen, setIsSupportOpen] = useState(false)
 
     useEffect(() => {
         if (data && data.response) {
@@ -16,6 +20,8 @@ export default function Topics() {
             setIsActive(data.response[0]);
         }
     }, []);
+
+    const supportToggle = () => setIsSupportOpen(!isSupportOpen)
 
     return (
         <div className={styles.topics}>
@@ -25,6 +31,8 @@ export default function Topics() {
                 <ActiveCourse isActive={isActive} />
             </div>
             <AsideChat />
+            <img className={styles.topics__support} src={isSupportOpen ? close_support_icon : support_icon} onClick={supportToggle} alt="Support" />
+            {isSupportOpen && <SupportWindow styles={styles} />}
         </div>
-    );
+    )
 }
