19419e339894fd7bfb2598094bfeb0a2a4699d68
Morzh7771 <imhoserge@gmail.com>
2024-08-18 15:59:52 +0300

    shit

--- src/components/main/Topics/CourseBar/index.jsx
+++ src/components/main/Topics/CourseBar/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import styles from './courseBar.module.scss';
 import document from '../../../../assets/icons/text_icon_b.svg';
+import arrow from '../../../../assets/icons/open_topic.svg';
 
 export default function CourseBar({ setIsActive, isActive, coursesData }) {
     const [activeCourses, setActiveCourses] = useState([]);
@@ -23,12 +24,14 @@ export default function CourseBar({ setIsActive, isActive, coursesData }) {
                     >
                         <img src={document} alt="" />
                         <span>{course.title}</span>
+                        <img src={arrow} alt="" className={styles.courseBar__arrow} />
                     </div>
                     {activeCourses.includes(course) && (
                         <div className={styles.subtopics}>
                             {course.subtopics.map((subtopic, subIndex) => (
                                 <div key={subIndex} className={styles.courseBar__topic}>
                                     <span onClick={() => setIsActive(subtopic)}> {subtopic.title}</span>
+                                    
                                 </div>
                             ))}
                         </div>
