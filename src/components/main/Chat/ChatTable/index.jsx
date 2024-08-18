f1448d6398620f59f498e0b0c53de0e5dbbda153
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-08-18 23:02:50 +0300

    fix

--- src/components/main/Chat/ChatTable/index.jsx
+++ src/components/main/Chat/ChatTable/index.jsx
@@ -3,59 +3,63 @@ import styles from './chatTable.module.scss';
 import arrow from '../../../../assets/icons/open_topic.svg';
 
 const ChatTable = ({ data }) => {
-  const [openBlocks, setOpenBlocks] = useState([]);
+    const [openBlocks, setOpenBlocks] = useState([]);
 
-  const toggleBlock = (blockTitle) => {
-    if (openBlocks.includes(blockTitle)) {
-      setOpenBlocks(openBlocks.filter((title) => title !== blockTitle));
-    } else {
-      setOpenBlocks([...openBlocks, blockTitle]);
-    }
-  };
+    const toggleBlock = (blockTitle) => {
+        if (openBlocks.includes(blockTitle)) {
+            setOpenBlocks(openBlocks.filter((title) => title !== blockTitle));
+        } else {
+            setOpenBlocks([...openBlocks, blockTitle]);
+        }
+    };
 
-  const table = useMemo(
-    () => (
-      <table className={styles.chatTable}>
-        <colgroup>
-          <col />
-          <col />
-          <col />
-          <col />
-        </colgroup>
-        <thead>
-          <tr>
-            <th>№</th>
-            <th>Title</th>
-            <th>Subtopics</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.keys(data).map((blockTitle, index) => (
-            <React.Fragment key={index}>
-              <tr onClick={() => toggleBlock(blockTitle)} className={styles.blockRow}>
-                <td>{index + 1}</td>
-                <td>{blockTitle}</td>
-                <td>{data[blockTitle].length} Subtopics</td>
-                <td>
-                  <img src={arrow} alt="" />
-                </td>
-              </tr>
-              {openBlocks.includes(blockTitle) &&
-                data[blockTitle].map((topic, topicIndex) => (
-                  <tr key={`${index}-${topicIndex}`} className={`${styles.subRow} ${styles.animate}`}>
-                    <td>{index + 1}.{topicIndex + 1}</td>
-                    <td colSpan="2">{topic}</td>
-                  </tr>
-                ))}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
-    ),
-    [data, openBlocks]
-  );
+    const table = useMemo(
+        () => (
+            <table className={styles.chatTable}>
+                <colgroup>
+                    <col />
+                    <col />
+                    <col />
+                    <col />
+                </colgroup>
+                <thead>
+                    <tr>
+                        <th>№</th>
+                        <th>Title</th>
+                        <th>Subtopics</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(data).map((blockTitle, index) => (
+                        <React.Fragment key={index}>
+                            <tr onClick={() => toggleBlock(blockTitle)} className={styles.blockRow}>
+                                <td>{index + 1}</td>
+                                <td>{blockTitle}</td>
+                                <td>{data[blockTitle].length} Subtopics</td>
+                                <td>
+                                    <img
+                                        src={arrow}
+                                        alt=""
+                                        className={`${styles.blockRow__arrow} ${openBlocks.includes(blockTitle) ? styles.blockRow__arrow__open : styles.blockRow__arrow__closed}`}
+                                    />
+                                </td>
+                            </tr>
+                            {openBlocks.includes(blockTitle) &&
+                                data[blockTitle].map((topic, topicIndex) => (
+                                    <tr key={`${index}-${topicIndex}`} className={`${styles.subRow} ${styles.animate}`}>
+                                        <td>{index + 1}.{topicIndex + 1}</td>
+                                        <td colSpan="2">{topic}</td>
+                                    </tr>
+                                ))}
+                        </React.Fragment>
+                    ))}
+                </tbody>
+            </table>
+        ),
+        [data, openBlocks]
+    );
 
-  return <div>{table}</div>;
+    return <div>{table}</div>;
 };
 
 export default ChatTable;
