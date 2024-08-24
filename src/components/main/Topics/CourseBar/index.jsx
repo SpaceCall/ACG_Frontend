744f8d952303f73b2872737dbd74ecf8e6fdde96
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-08-24 10:28:08 +0300

    smooth course bar

--- src/components/main/Topics/CourseBar/index.jsx
+++ src/components/main/Topics/CourseBar/index.jsx
@@ -1,50 +1,57 @@
-import React, { useState } from 'react'
-import styles from './courseBar.module.scss'
-import tick from '../../../../assets/icons/complete_icon.svg'
-import document from '../../../../assets/icons/text_icon_b.svg'
-import arrow from '../../../../assets/icons/open_topic.svg'
+import React, { useState } from 'react';
+import styles from './courseBar.module.scss';
+import tick from '../../../../assets/icons/complete_icon.svg';
+import document from '../../../../assets/icons/text_icon_b.svg';
+import arrow from '../../../../assets/icons/open_topic.svg';
 
 export default function CourseBar({ setIsActive, isActive, coursesData }) {
-    const [activeCourses, setActiveCourses] = useState([])
+    const [activeCourses, setActiveCourses] = useState([]);
 
-    const handleCourseClick = (course) => activeCourses.includes(course) ? setActiveCourses(activeCourses.filter((active) => active !== course)) : setActiveCourses([...activeCourses, course])
+    const handleCourseClick = (course) => {
+        setActiveCourses((prevActiveCourses) =>
+            prevActiveCourses.includes(course)
+                ? prevActiveCourses.filter((active) => active !== course)
+                : [...prevActiveCourses, course]
+        );
+    };
 
     return (
         <div className={styles.courseBar}>
             {coursesData && coursesData.map((course, index) => (
-                <div key={index}>
+                <section key={index} className={styles.courseBar__section}>
                     <div
-                        className={`${styles.courseBar__topic}`}
+                        className={styles.courseBar__heading}
                         onClick={() => handleCourseClick(course)}
                     >
-                        <div className={styles.courseBar__topic__left}>
-                            <img src={tick} alt="" />
-                            <div className={styles.courseBar__topic__left__info}>
-                                <span>{course.title}</span>
-                                <span>Video: 10 min</span>
-                            </div>
-                        </div>
-                        <img
-                            src={arrow}
-                            alt=""
-                            className={`${styles.courseBar__arrow} ${activeCourses.includes(course) ? styles.courseBar__arrow__open : styles.courseBar__arrow__closed}`}
-                        />
-                    </div>
-                    {activeCourses.includes(course) && (
-                        <div className={styles.subtopics}>
-                            {course.subtopics.map((subtopic, subIndex) => (
-                                <div
-                                    key={subIndex}
-                                    onClick={() => setIsActive(subtopic)}
-                                    className={`${styles.courseBar__subtopic} ${isActive && isActive.title === subtopic.title ? styles.active : ''}`}>
-                                    <img src={document} alt="" />
-                                    <span>{subtopic.title}</span>
+                        <div className={styles.courseBar__topic}>
+                            <div className={styles.courseBar__topic__left}>
+                                <img src={tick} alt="" />
+                                <div className={styles.courseBar__topic__left__info}>
+                                    <span>{course.title}</span>
+                                    <span>Video: 10 min</span>
                                 </div>
-                            ))}
+                            </div>
+                            <img
+                                src={arrow}
+                                alt=""
+                                className={`${styles.courseBar__arrow} ${activeCourses.includes(course) ? styles.courseBar__arrow__open : styles.courseBar__arrow__closed}`}
+                            />
                         </div>
-                    )}
-                </div>
+                    </div>
+                    <div className={`${styles.courseBar__content} ${activeCourses.includes(course) ? styles.open : ''}`}>
+                        {course.subtopics.map((subtopic, subIndex) => (
+                            <div
+                                key={subIndex}
+                                onClick={() => setIsActive(subtopic)}
+                                className={`${styles.courseBar__subtopic} ${isActive && isActive.title === subtopic.title ? styles.active : ''}`}
+                            >
+                                <img src={document} alt="" />
+                                <span>{subtopic.title}</span>
+                            </div>
+                        ))}
+                    </div>
+                </section>
             ))}
         </div>
-    )
+    );
 }
